3d0adef535480749ef1fbafd6e4c8dd4da08a687
ibrahim3929 <muhammed004414@gmail.com>
2025-04-26 02:08:54 +0530

    Fix: set host and port for Render

--- app.py
+++ app.py
@@ -90,5 +90,9 @@ def index():
 
     return render_template('index.html', countries=sorted(MINIMUM_WAGES.keys()))
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
